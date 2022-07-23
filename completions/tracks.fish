f0cf577643002850f9510eb56e440819d57a6d3a
Colerar <233hbj@gmail.com>
2022-07-23 20:48:32 +0800

    feat: update completions

--- completions/tracks.fish
+++ completions/tracks.fish
@@ -3,7 +3,7 @@
 
 
 ### Setup for tracks
-set -l tracks_subcommands 'dig loginqr config version'
+set -l tracks_subcommands 'dig login config version'
 
 ## Options for tracks
 complete -c tracks -n "not __fish_seen_subcommand_from $tracks_subcommands" -s g -o generate-completion -d '为 bash|zsh|fish 生成补全文件'
@@ -24,7 +24,7 @@ complete -c tracks -n "__fish_seen_subcommand_from dig" -o multipart -o mt -r -d
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o qe -o qn -o qnd -o quality-exact -o quality-near -o quality-near-down -d '质量匹配模式, 可选精确或临近模式, 默认向上临近'
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o qv -o video-quality -r -d '视频质量, 支持 360P 到 8K, 可搭配 -quality-xxx 使用, 可用选项: [240p,360p,480p,720p,720p60f,1080p,1080plus,1080p60f,4k,hdr,dolby,8k]'
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o videocodec -o codec -o cv -r -d '视频编码优先级, 默认 [avc, hevc, av1], 可用 [avc,hevc,av1,h264,h265,h.264,h.265]'
-complete -c tracks -n "__fish_seen_subcommand_from dig" -o qa -o audio-quality -r -d '音频质量, 支持 64kbps - 320kbps 以及 dolby e-ac-3, 可搭配 -quality-xxx使用, 可用选项: low, medium, high, dolby'
+complete -c tracks -n "__fish_seen_subcommand_from dig" -o qa -o audio-quality -r -d '音频质量, 支持 64kbps - 320kbps 以及 dolby e-ac-3, 可搭配 -quality-xxx使用, 可用选项: low, medium, high, dolby, flac'
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o sub-lang -o sl -r -d '要下载的字幕的语言代码(如 zh-hant, zh-hans), 默认中文, 可指定多个, all 为全部'
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o sub-loose -o sub-loose-match -o slm -o sub-strict -o ss -d '是否开启宽松模式, 不仅将匹配语言代码, 同时也匹配语言名称, 并且仅要求匹配项包含, 而非相等, 默认开启'
 complete -c tracks -n "__fish_seen_subcommand_from dig" -o sub-weird -o sw -o sub-fallback -o sf -d '字幕贪婪 / 回退匹配模式, 默认回退, 前者将根据指定的顺序选定, 至多选择一个; 后者将下载所有匹配的字幕'
@@ -42,11 +42,12 @@ complete -c tracks -n "__fish_seen_subcommand_from dig" -s h -l help -d 'Show th
 complete -c tracks -n "__fish_seen_subcommand_from dig" -d 'B 站视频地址或 av BV ss ep md 等号码'
 
 
-### Setup for loginqr
-complete -c tracks -f -n __fish_use_subcommand -a loginqr -d '扫码登录'
+### Setup for login
+complete -c tracks -f -n __fish_use_subcommand -a login -d '扫码登录'
 
-## Options for loginqr
-complete -c tracks -n "__fish_seen_subcommand_from loginqr" -s h -l help -d 'Show this message and exit'
+## Options for login
+complete -c tracks -n "__fish_seen_subcommand_from login" -o sms -o qr -o cookie -o pwd -d '登录的方式，包括可以使用 [-sms, -qr, -cookie, -pwd]'
+complete -c tracks -n "__fish_seen_subcommand_from login" -s h -l help -d 'Show this message and exit'
 
 
 ### Setup for config
